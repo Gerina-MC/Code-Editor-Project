70388b68ca969a35bad3b4eb06ba099f819a5d30
Gerina-MC <gerinamary7133@gmail.com>
2022-12-31 20:17:26 +0530

    S com

--- jsbinjquery.js
+++ jsbinjquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var wid=screen.width-80;
-    $("#html").width(wid/3);
-    $("#css").width(wid/3);
-    $("#js").width(wid/3);
+    var wid=screen.width;
+    $("#html").width((wid/2)-20);
+    $("#css").width((wid/3)-20);
+    $("#js").width((wid/3)-20);
 });
