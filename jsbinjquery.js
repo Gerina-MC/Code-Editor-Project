f72de4826d00f3f84c817630909101dbd587d60f
Gerina-MC <gerinamary7133@gmail.com>
2023-01-02 11:41:40 +0530

    Try

--- jsbinjquery.js
+++ jsbinjquery.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    var wid=screen.width;
-    $("#html").width((wid/2)-20);
-    $("#css").width((wid/3)-20);
-    $("#js").width((wid/3)-20);
+    var wid=screen.width-40;
+    var hei=screen.height-100;
+    $("#html").width((wid/2));
+    $("#css").width((wid)/2);
+    $("#js").width((wid)/3);
+    $("#html").height((hei/2));
+    $("#css").height((hei/2));
+    $("#js").height((hei/2));
+    $("#bht").click(function() {
+        $("#dht").slideToggle(0.0000000000001);
+    })
+    $("#bcs").click(function() {
+        $("#dcs").slideToggle(0.0000000000001);
+    })
+    $("#bjs").click(function() {
+        $("#djs").slideToggle(0.0000000000001);
+    })
+    $("#bre").click(function() {
+        $("#dre").slideToggle(0.0000000000001);
+    })
 });
