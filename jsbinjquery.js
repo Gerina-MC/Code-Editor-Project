0dd119327a7b555f0c84ffea0808614a06b2fa77
Gerina-MC <gerinamary7133@gmail.com>
2023-01-07 15:52:22 +0530

    Added a lot

--- jsbinjquery.js
+++ jsbinjquery.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var wid=$(window).width()-35;
-    var hei=$(window).height()-$('.navbar').height()-60;
+    var hei=$(window).height()-$('.navbar').height()-59;
     var h=$("#html");
     var c=$("#css");
     var j=$("#js");
@@ -12,7 +12,7 @@ $(document).ready(function() {
     h.width(wid/3);
     c.width(wid/3);
     j.width(wid/3);
-    f.width(wid);
+    f.width(wid+18);
     var f1=1,f2=1,f3=1,f4=1,fl=0;
     $('#rst').click(function(){
         $("#html").val("");
@@ -37,7 +37,7 @@ $(document).ready(function() {
                 $("#djs").slideToggle(0.0000000000001);
                 f3=0;
             }
-            h.width(wid);
+            h.width(wid+12);
             if(f1==0)
             {
                 $("#bht").css('background-color','#2196F3');
@@ -54,14 +54,14 @@ $(document).ready(function() {
             }
             h.height(hei/2);
             f.height(hei/2);
+            $("#bcs").prop('disabled', true);
+            $("#bjs").prop('disabled', true);
             $("#bcs").css('background-color','grey');
             $("#bcs").css('color','black');
             $("#bcs").css('border','2px solid grey');
             $("#bjs").css('background-color','grey');
             $("#bjs").css('color','black');
             $("#bjs").css('border','2px solid grey');
-            $("#bcs").prop('disabled', true);
-            $("#bjs").prop('disabled', true);
         }
         else {
             fl=0;
@@ -89,10 +89,10 @@ $(document).ready(function() {
             }
             $("#bcs").css('background-color','#2196F3');
             $("#bcs").css('color','white');
-            $("#bcs").css('border','2px solid #2196F3');
+            $("#bcs").css('border','2px solid #2196f3');
             $("#bjs").css('background-color','#2196F3');
             $("#bjs").css('color','white');
-            $("#bjs").css('border','2px solid #2196F3');
+            $("#bjs").css('border','2px solid #2196f3');
             $("#dcs").slideToggle(0.0000000000001);
             $("#djs").slideToggle(0.0000000000001);
             f2=1;
@@ -110,16 +110,16 @@ $(document).ready(function() {
             f1=0;
             if(f2==1 && f3==1)
             {
-                c.width(wid/2);
-                j.width(wid/2);
+                c.width((wid/2)+3);
+                j.width((wid/2)+3);
             }
             else if(f2==0 && f3==1)
             {
-                j.width(wid);
+                j.width(wid+12);
             }
             else if(f2==1 && f3==0)
             {
-                c.width(wid);
+                c.width(wid+12);
             }
             else
             {
@@ -137,6 +137,7 @@ $(document).ready(function() {
             if(f4==1)
             {
                 h.height(hei/2);
+                f.height(hei/2);
             }
             else
             {
@@ -150,17 +151,17 @@ $(document).ready(function() {
             }
             else if(f2==0 && f3==1)
             {
-                h.width(wid/2);
-                j.width(wid/2);
+                h.width((wid/2)+3);
+                j.width((wid/2)+3);
             }
             else if(f2==1 && f3==0)
             {
-                h.width(wid/2);
-                c.width(wid/2);
+                h.width((wid/2)+3);
+                c.width((wid/2)+3);
             }
             else
             {
-                h.width(wid);
+                h.width(wid+12);
             }
         }
     });
@@ -173,16 +174,16 @@ $(document).ready(function() {
             f2=0;
             if(f1==1 && f3==1)
             {
-                h.width(wid/2);
-                j.width(wid/2);
+                h.width((wid/2)+3);
+                j.width((wid/2)+3);
             }
             else if(f1==0 && f3==1)
             {
-                j.width(wid);
+                j.width(wid+12);
             }
             else if(f1==1 && f3==0)
             {
-                h.width(wid);
+                h.width(wid+12);
             }
             else
             {
@@ -200,6 +201,7 @@ $(document).ready(function() {
             if(f4==1)
             {
                 c.height(hei/2);
+                f.height(hei/2);
             }
             else
             {
@@ -213,17 +215,17 @@ $(document).ready(function() {
             }
             else if(f1==0 && f3==1)
             {
-                c.width(wid/2);
-                j.width(wid/2);
+                c.width((wid/2)+3);
+                j.width((wid/2)+3);
             }
             else if(f1==1 && f3==0)
             {
-                h.width(wid/2);
-                c.width(wid/2);
+                h.width((wid/2)+3);
+                c.width((wid/2)+3);
             }
             else
             {
-                c.width(wid);
+                c.width(wid+12);
             }
         }
     });
@@ -236,16 +238,16 @@ $(document).ready(function() {
             f3=0;
             if(f1==1 && f2==1)
             {
-                h.width(wid/2);
-                c.width(wid/2);
+                h.width((wid/2)+3);
+                c.width((wid/2)+3);
             }
             else if(f1==0 && f2==1)
             {
-                c.width(wid);
+                c.width(wid+12);
             }
             else if(f1==1 && f2==0)
             {
-                h.width(wid);
+                h.width(wid+12);
             }
             else
             {
@@ -263,6 +265,7 @@ $(document).ready(function() {
             if(f4==1)
             {
                 j.height(hei/2);
+                f.height(hei/2);
             }
             else
             {
@@ -276,17 +279,17 @@ $(document).ready(function() {
             }
             else if(f1==0 && f2==1)
             {
-                c.width(wid/2);
-                j.width(wid/2);
+                c.width((wid/2)+3);
+                j.width((wid/2)+3);
             }
             else if(f1==1 && f2==0)
             {
-                h.width(wid/2);
-                j.width(wid/2);
+                h.width((wid/2)+3);
+                j.width((wid/2)+3);
             }
             else
             {
-                j.width(wid);
+                j.width(wid+12);
             }
         }
     });
