f7e1c0962f1d3bf52ea29ba027e028d27cde24cc
Gerina-MC <gerinamary7133@gmail.com>
2023-01-08 10:49:40 +0530

    Adjusted width

--- jsbinjquery.js
+++ jsbinjquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var wid=$(window).width()-35;
+    var wid=$(window).width()-50;
     var hei=$(window).height()-$('.navbar').height()-59;
     var h=$("#html");
     var c=$("#css");
